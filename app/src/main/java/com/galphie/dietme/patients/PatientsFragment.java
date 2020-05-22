f92843d9ee1577f4907afb6a85ad6bc45033b87d
Alfonso Gil <algparis96@gmail.com>
2020-05-22 21:12:11 +0200

    Fragmento Pacientes | Adaptador con listener para eventos click                     | Layout del bottom_nav corregido

--- app/src/main/java/com/galphie/dietme/patients/PatientsFragment.java
+++ app/src/main/java/com/galphie/dietme/patients/PatientsFragment.java
@@ -1,4 +1,4 @@
-package com.galphie.dietme;
+package com.galphie.dietme.patients;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -6,16 +6,23 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.galphie.dietme.R;
+import com.galphie.dietme.User;
+import com.galphie.dietme.Utils;
 
 import java.util.ArrayList;
 
-public class PatientsFragment extends Fragment {
+public class PatientsFragment extends Fragment implements PatientsListAdapter.OnPatientClickListener {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
     private String mParam1;
     private String mParam2;
     private ArrayList<User> patientsList;
+    private RecyclerView recyclerView;
 
     public PatientsFragment() {
     }
@@ -43,6 +50,22 @@ public class PatientsFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_patients, container, false);
 
+        recyclerView = (RecyclerView) view.findViewById(R.id.patients_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        PatientsListAdapter adapter = new PatientsListAdapter(patientsList,this);
+        recyclerView.setAdapter(adapter);
+
         return view;
+    }
+
+    @Override
+    public void onPatientClick(int position) {
+        Utils.toast(getActivity().getApplicationContext()
+                , "Has tocado a " + patientsList.get(position).getName() + " " + patientsList.get(position).getForenames());
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
     }
 }
