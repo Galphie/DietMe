eade4f2c6f767a39065aa4e094a261d43bac6161
Alfonso Gil <algparis96@gmail.com>
2020-05-26 00:27:15 +0200

    Información de paciente | PagerView implementado

---
+++ app/src/main/java/com/galphie/dietme/patients/SlidePagerAdapter.java
@@ -0,0 +1,29 @@
+package com.galphie.dietme.patients;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+
+
+public class SlidePagerAdapter extends FragmentStatePagerAdapter {
+    private ArrayList<Fragment> fragments;
+
+    public SlidePagerAdapter(@NonNull FragmentManager fm, ArrayList<Fragment> fragments) {
+        super(fm);
+        this.fragments = fragments;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+}
